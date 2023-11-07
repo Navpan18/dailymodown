10939f30344e82f643db354f8e6713af9758c915
Navpan18 <npanchayan.official@gmail.com>
2023-11-07 10:32:33 +0530

    main

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from fastapi import FastAPI
 import youtube_dl
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
-    video_url = 'https://www.dailymotion.com/video/x8pdo3p'
-    desired_format = 'http-720-0'
+async def root():
+    return {"message": "Hello World"}
+@app.get("/dailymo/")
+async def read_item(reqUrl: str = 'https://www.dailymotion.com/video/x8pdo3p', vidFormat: str='http-720-0'):
+    
+    video_url = reqUrl
+    desired_format = vidFormat
     ydl_opts = {
         'quiet': True,  
         'format': desired_format,  
@@ -17,6 +22,6 @@ def read_root():
     info_dict = ydl.extract_info(video_url, download=False)
     format_url = info_dict.get('url')
     if format_url:
-        return {"url":format_url}
+        return {"url":format_url,"reqUrl":reqUrl,"vidFormat":vidFormat}
     else:
-        return {"url":"Not found"}   
+        return {"url":"Not found","reqUrl":reqUrl,"vidFormat":vidFormat}   
