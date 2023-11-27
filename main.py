56ca9d3f0aa2afb1d8705039ca838d4e579e6ffb
Navneet Panchayan <npanchayan.official@gmail.com>
2023-11-27 23:38:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 import youtube_dl
 app = FastAPI()
-
+def get_video_formats(video_url):
+    formats_list = []
+    
+    with youtube_dl.YoutubeDL() as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        for format in info_dict.get('formats', []):
+            format_info = {
+                'format_id': format['format_id'],
+                'ext': format['ext'],
+                'resolution': format.get('resolution', ''),
+            }
+            formats_list.append(format_info)
+    
+    return formats_list
 
 @app.get("/")
 async def root():
@@ -22,4 +35,9 @@ async def read_item(reqUrl: str = 'https://www.dailymotion.com/video/x8pywti', v
     if format_url:
         return {"url":format_url,"reqUrl":reqUrl,"vidFormat":vidFormat}
     else:
-        return {"url":"Not found","reqUrl":reqUrl,"vidFormat":vidFormat}   
+        return {"url":"Not found","reqUrl":reqUrl,"vidFormat":vidFormat}  
+@app.get("/getf/")
+async def read_item(videourl: str = 'https://www.dailymotion.com/video/x8pywti'):
+    
+    available_formats = get_video_formats(videourl)
+    return {"video_url": videourl, "formats": available_formats}
